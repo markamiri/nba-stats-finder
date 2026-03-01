9a4ebae38959217dd757773dc32586c7100c39bd
markamiri <yimminglu@gmail.com>
2026-03-01 15:46:33 -0500

    fixing cors issue on prod

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5174",
-        "http://127.0.0.1:5174"
+        "http://127.0.0.1:5174",
+        "https://nba-frontend-beta.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
