b0c197d80ac16c40361cde2553b096e8606977e5
markamiri <yimminglu@gmail.com>
2026-03-03 22:20:59 -0500

    fixing SSL connection has been closed unexpectedly

--- db.py
+++ db.py
@@ -9,4 +9,8 @@ DATABASE_URL = os.getenv("NEON_CONNECTION_STRING")
 if not DATABASE_URL:
     raise ValueError("NEON_CONNECTION_STRING not set")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,     # checks if connection is alive
+    pool_recycle=300        # refresh connection every 5 min
+)
